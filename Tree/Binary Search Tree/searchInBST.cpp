0f6cbbe906d38b88e8291b10256c87878c26df66
Zaid khan <itszaidkhan01@gmail.com>
2025-12-11 19:06:22 +0530

    Added programs of binary search tree

---
+++ Tree/Binary Search Tree/searchInBST.cpp
@@ -0,0 +1,86 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+class Node{
+public:
+    int data;
+    Node* left;
+    Node* right;
+
+    Node(int val){
+        data = val;
+        left = right = NULL;
+    }
+};
+
+// function for inserting node in a BST
+Node* insert(Node* root, int val){
+    if(root == NULL){
+        return new Node(val);
+    }
+
+    if(val<root->data){
+        root->left = insert(root->left, val);
+    }
+    else{
+        root->right = insert(root->right, val);
+    }
+
+    return root;
+}
+
+// function for building BST
+static int idx = -1; // pointer that traverses whole array
+Node* buildBST(vector<int> arr){
+    Node* root = NULL;
+    for(int val: arr){
+        root = insert(root, val);
+    }
+
+    return root;
+}
+
+// function for search an element in BST
+bool search(Node* root, int key){
+    if(root == NULL){
+        return false;
+    }
+    if(root->data == key) return true;
+
+    if(root->data > key){
+        return search(root->left, key);
+    }
+    else{
+        return search(root->right, key);
+    }
+}
+
+// function for inorder traversal
+void inOrder(Node* root){
+    if(root == NULL) return; // backtrack
+
+    inOrder(root->left); // traverse left subtree
+    cout<<root->data<<" "; // traverse parent node
+    inOrder(root->right); // traverse right subtree
+}
+
+int main(int argc, char const *argv[])
+{
+    vector<int> arr = {3, 2, 1, 5, 6, 4};
+    int elem;
+    Node* root = buildBST(arr);
+    inOrder(root);
+    cout<<endl;
+    
+    cout<<"Enter element you want to search : ";
+    cin>>elem;
+
+    if(search(root, elem)){
+        cout<<"Element found!";
+    }
+    else{
+        cout<<"Element not found!";
+    }
+    return 0;
+}
