1173b1d49abdf694668edb1e6b436038b4bc9333
Zaid khan <itszaidkhan01@gmail.com>
2026-01-18 20:35:48 +0530

    First program of Graph i.e how to build using dynamic array (list)

---
+++ Graphs/buildingAGraph.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <list>
+using namespace std;
+
+class Graph
+{
+    int V;
+    list<int> *l; // dynamic array for storing neighbors of particular vertex
+
+public:
+    Graph(int V)
+    {
+        this->V = V;
+        l = new list<int>[V];
+    }
+
+    // add edge between vertex = storing in adjacency list
+    void addEdge(int u, int v)
+    {
+        /*
+        for undirected graph, we know that both vertex that are connected through an edge stores each other
+        in their own adjacency list
+        */
+        l[u].push_back(v);
+        l[v].push_back(u);
+    }
+
+    // function for printing adjacency list of individual vertex
+    void printAdjList()
+    {
+        cout << "Adjacency list of this graph is : " << endl;
+        for (int i = 0; i < V; i++)
+        {
+            cout << i << " : ";
+            for (int neigh : l[i])
+            {
+                cout << neigh << " ";
+            }
+            cout << endl;
+        }
+    }
+};
+
+int main(int argc, char const *argv[])
+{
+    Graph g(5);
+
+    g.addEdge(0, 1);
+    g.addEdge(1, 2);
+    g.addEdge(1, 3);
+    g.addEdge(2, 3);
+    g.addEdge(2, 4);
+
+    g.printAdjList();
+    return 0;
+}
