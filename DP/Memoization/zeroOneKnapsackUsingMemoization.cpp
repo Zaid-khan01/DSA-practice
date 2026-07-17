06262d7e0c7aa6925da936cbd5c3394c4419e10a
Zaid khan <itszaidkhan01@gmail.com>
2026-07-17 22:39:29 +0530

    Added 0/1 knapsack using recursion & memoization

---
+++ DP/Memoization/zeroOneKnapsackUsingMemoization.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+static int t[100][100];
+int knapsack(int wt[], int val[], int w, int n){
+    if(n == 0 || w == 0){
+        return 0;
+    }
+    if(t[n][w] != -1){
+        return t[n][w];
+    }
+    if(wt[n-1] <= w){
+        return t[n][w] = max(val[n-1] + knapsack(wt, val, w-wt[n-1], n-1), 
+                    knapsack(wt, val, w, n-1));
+    }
+    
+    else{
+        return t[n][w] = knapsack(wt, val, w, n-1);
+    }
+}
+int main() {
+    memset(t, -1, sizeof(t));
+    int wt[4] = {1, 3, 4, 5};
+    int val[4] = {1, 4, 5, 7};
+    int w = 7;
+    int profit = knapsack(wt, val, w, 4);
+    cout<<"Maximum profit is = "<<profit;
+    return 0;
+}
